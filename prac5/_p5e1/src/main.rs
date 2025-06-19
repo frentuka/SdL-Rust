787661f421b17ab92b6242b4aef706d1c7e1a923
srleg <frentuka@gmail.com>
2025-06-18 21:22:59 -0300

    ...

---
+++ prac5/_p5e1/src/main.rs
@@ -0,0 +1,19 @@
+pub mod structs;
+
+/*
+a- Al agregar un auto si supera el límite de la concesionaria debe arrojar un error propio con un mensaje de contexto.
+
+b- Haga todos los tests correspondientes para probar en profundidad los métodos que agregan un auto y eliminan un auto de la concesionaria,
+    obteniendo el mayor porcentaje de coverage sobre el código que realiza las operaciones.
+
+c- Una vez hecho el punto anterior debe hacer que los autos de la concesionaria se almacenen en un archivo en formato JSON.
+    Agregue y modifique lo que considere necesario para que:
+        - Al agregar un nuevo auto se abre el archivo de autos guardados y lo agregue a dicho archivo.
+        - Eliminar un auto: al eliminar un auto se debe eliminar este del archivo.
+
+No debe modificar los tests hechos en el punto b. Si puede agregar más en caso de que haga nueva funcionalidad..
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ prac5/_p5e2/src/main.rs
@@ -0,0 +1,14 @@
+// 2- En base al ejercicio 8 del tp#3 implemente lo siguiente:
+//      a- Realice todos los tests de la funcionalidad implementada obteniendo un coverage de por lo menos 90%
+//      b- Una vez obtenido dicho coverage, las canciones de la playlist deben ser guardadas en un archivo en formato JSON,
+//          por lo tanto las operaciones que agreguen, quiten o modifiquen la playlist deben estar respaldadas sobre dicho archivo.
+//      
+//      No debe modificar los tests hechos en el punto a.
+//      Si puede agregar más en caso de que haga métodos nuevos.
+//      Recuerde también que se debe seguir manteniendo un coverage de al menos 90%,
+
+mod structs;
+
+fn main() {
+    println!("Hello, world!");
+}
